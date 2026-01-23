9ed09698c1e915b07e94827985fff2ebe0b917ec
Reza Indrawan <me.rezaindrawan@gmail.com>
2026-01-23 15:24:39 +0700

    feat - clean project

--- example/android/app/src/main/kotlin/com/rezins/flutter_face_ai_sdk_example/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.rezins.flutter_face_ai_sdk_example
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- lib/flutter_face_ai_sdk.dart
+++
@@ -1,8 +0,0 @@
-
-import 'flutter_face_ai_sdk_platform_interface.dart';
-
-class FlutterFaceAiSdk {
-  Future<String?> getPlatformVersion() {
-    return FlutterFaceAiSdkPlatform.instance.getPlatformVersion();
-  }
-}
